67dac2615357fa3e295ea90496ab05316026b436
Wu Xiaotian <yetist@gmail.com>
2015-01-06 00:08:55 +0800

    test template for i18n

--- i18n/example/main.go
+++ i18n/example/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"github.com/chai2010/gettext-go/gettext"
 	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
 	"github.com/yetist/middleware/i18n"
+	"html/template"
 )
 
 func __(msgid string) string {
@@ -12,12 +14,28 @@ func __(msgid string) string {
 
 func main() {
 	m := martini.Classic()
+	m.Use(render.Renderer(render.Options{
+		Directory: "templates",
+		Funcs: []template.FuncMap{
+			{
+				"__": func(s string) string {
+					return __(s)
+				},
+			},
+		},
+	}))
 	m.Use(i18n.I18n(i18n.Options{
 		Domain:    "example",
 		Parameter: "lang",
 	}))
+
 	m.Get("/", func() string {
 		return __("Hello world!")
+	})
+
+	// template
+	m.Get("/hi", func(r render.Render) {
+		r.HTML(200, "index", __("Hello world!"))
 	})
 	m.Run()
 }
